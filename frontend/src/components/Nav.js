b5210d840bbd4994c446973ca75392ba7dd3622d
Oskar Gawryszewski <oskargawryszewski@MacBook-Air-Oskar-2.local>
2024-01-03 22:24:43 +0100

    dodalem stylowanie strony glownej i navbar

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -3,17 +3,19 @@ import whiteLogo from '../images/tinder_logo_white.png';
 import colorLogo from "../images/color-logo-tinder.png";
 import Image from 'next/image'
 
-function Nav({minimal}) {
+function Nav({minimal, authToken}) {
 
   return (
     <nav>
         <div className="logo-container">
         <Image
-          className="logo"
-          src={minimal ? colorLogo : whiteLogo}
-          alt="logo"
+            className="logo"
+            src={minimal ? colorLogo : whiteLogo}
+            alt="logo"
         />
         </div>
+
+        {!authToken && !minimal  && <button className='nav-button'>Log in</button>}
     </nav>
   )
 }
--- frontend/src/pages/homePage.js
+++ frontend/src/pages/homePage.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Nav from '../components/Nav'
 
 function HomePage() {
+    const [showModal, setShowModal] = useState(false);
     const authToken = false;
 
     const handleClick = () => {
         console.log('clicked')
+        setShowModal(true);
     }
 
   return (
     <>
-    <Nav minimal={false}/>
+    <div className="overlay">
+    <Nav minimal={false} authToken={authToken}/>
     <div className='home'>
         <h1>Swipe Right</h1>
         <button className='primary-button' onClick={handleClick}>
             {authToken ? 'Signout' : 'Create account'}
         </button>
+    </div>
     </div>
     </>
   )
